78c2264cc1b2c8aaf9c24301b7bd5b64d4f3b90c
Gabriel <gabrielborges031807@gmail.com>
2022-10-11 18:04:17 -0300

    AppDependencies now implements blocDependencies

--- gbx_di/lib/src/dependencies/app_dependencies.dart
+++ gbx_di/lib/src/dependencies/app_dependencies.dart
@@ -1,8 +1,10 @@
 import 'dart:async';
 
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:gbx_di/gbx_di.dart';
 
-abstract class AppDependencies extends Dependencies {
+abstract class AppDependencies extends Dependencies
+    implements BlocDependencies {
   AppDependencies();
 
   bool get injected => _completer.isCompleted;
@@ -15,6 +17,19 @@ abstract class AppDependencies extends Dependencies {
       await dependency.inject();
     }
     _completer.complete(true);
+  }
+
+  @override
+  List<BlocProvider> getGlobalBlocProviders() {
+    var providers = <BlocProvider>[];
+
+    for (var dependency in dependencies) {
+      if (dependency is BlocDependencies) {
+        providers.addAll(dependency.getGlobalBlocProviders());
+      }
+    }
+
+    return providers;
   }
 
   List<Dependencies> get dependencies;
--- gbx_di/lib/src/dependencies/bloc_dependencies.dart
+++ gbx_di/lib/src/dependencies/bloc_dependencies.dart
@@ -4,14 +4,3 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 mixin BlocDependencies on Dependencies {
   List<BlocProvider> getGlobalBlocProviders();
 }
-mixin BlocAppDependencies on AppDependencies {
-  List<BlocProvider> getGlobalBlocProviders() {
-    var providers = <BlocProvider>[];
-    for (var dependency in dependencies) {
-      if (dependency is BlocDependencies) {
-        providers.addAll(dependency.getGlobalBlocProviders());
-      }
-    }
-    return providers;
-  }
-}
