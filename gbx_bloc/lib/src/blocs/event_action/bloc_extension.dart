3f075dc4151b977401d4e2d8d8bc6479fd604cdf
Gabriel <gabrielborges031807@gmail.com>
2022-11-05 17:53:47 -0300

    removed gbx_auth

--- gbx_bloc/lib/src/blocs/event_action/bloc_extension.dart
+++ gbx_bloc/lib/src/blocs/event_action/bloc_extension.dart
@@ -1,7 +1,7 @@
 part of 'event_action.dart';
 
 extension EventActions<E, S> on Bloc<E, S> {
-  void registerHandler<Event extends E>(EventAction<Event, S> handler) {
+  void registerAction<Event extends E>(EventAction<Event, S> handler) {
     on<Event>(
       (event, emit) => handler.execute(event, state, emit),
       transformer: handler.transformer,
